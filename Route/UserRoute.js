c03194c68dd7892b2719e0cc38721666680a30bd
Kingsley <sabistore12@gmail.com>
2019-10-04 16:29:50 +0100

    added a host meeting route and also created a unique id number

--- Route/UserRoute.js
+++ Route/UserRoute.js
@@ -1,12 +1,13 @@
 const express =  require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const UserModel = require('../Model/User');
 const bcrypt = require('bcrypt');
+var randomId = require('random-id');
 
 
-router.get('/', (req, res)=>{
-    res.send("Successful respond")
-})
+
+
 
 router.post('/', async (req, res)=>{
     try {
@@ -14,10 +15,11 @@ router.post('/', async (req, res)=>{
         const user = await UserModel.create(req.body);
         const result = user.toJSON();
         delete result.password;
+        const token =jwt.sign({id: user.id}, process.env.jwt_secret, {expiresIn: '2h'});
 
         res.status(200).json({
             status: 'Successful request',
-            data: {user: result}
+            data: {user: result, token}
         })
         res.send("successfull request made");
        
@@ -29,6 +31,85 @@ router.post('/', async (req, res)=>{
     }
     
 })
+
+
+router.post('/login', async (req, res)=>{
+    try {
+        const user = await UserModel.findOne({email:req.body.email}, '+password');
+
+        if(!user) return res.status(401).json({status: "error", message: "Invalid  detials"});
+
+        const ispassword = await bcrypt.compare(req.body.password, user.password);
+       
+
+        if(!ispassword) return res.json({status: 'error', message:"invalid login detials"});
+
+        const token = jwt.sign({id: user.id}, process.env.jwt_secret);
+
+        res.json({status: "success",data: {token}});
+
+
+    } catch (error) {
+        res.json({
+            status:'Error',
+            message:'Cannot get login name correctly'
+        })
+    }
+});
+
+router.post('/host',async (req, res)=>{
+    try {
+        let len = 5;
+        let pattern = 'Ae0';
+        let id =  randomId(len, pattern);
+        console.log(id)
+        
+
+        const host = await UserModel.find({email:req.body.email}) ;
+
+        if(!host) return res.status(401).json({status: "error", message: "Invalid  detials"});
+
+        /* const token = jwt.sign({id: id}, SECRET);
+        console.log(token); */
+        res.json({status: "success",data: {id}});
+
+
+    } catch (error) {
+        res.json({
+            status:'Error',
+            message:'Cannot get Host name correctly'
+        })
+    }
+  
+});
+
+router.get('/', async (req, res)=>{
+    try {
+        const authHeader = req.headers.authorization;
+        if(!authHeader) return res.status(404).json({status: "error", message:"please specify a header"});
+
+        const token = authHeader.split(' ')[1];
+
+        const tokenData = jwt.verify(token, process.env.jwt_secret);
+
+        const user = await UserModel.findById(tokenData.id);
+       res.json({
+            status: "success",
+            data: user
+        });
+
+        res.json({tokenData});
+
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+
+
+
+
 
 
 module.exports = router;
